fe3b716e5e92cd5c51475fcc13305911bb9d610a
weil0ng <cuiweilong0205@gmail.com>
2018-04-13 11:24:35 -0700

    Init from internal repo.

---
+++ interpreter/parser.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+import argparse
+from utils.charm_options import *
+from abstract_syntax_tree import *
+from interpreter import *
+from pyparsing import *
+
+ParserElement.setDefaultWhitespaceChars(' ')
+
+COND_EQ = re.compile(r',\s*(\w+)\s*=\s*(\w+)\s*\)')
+NAME_EXT = re.compile(r'(?![\d\.+])([\w]+)\.([\w\.]+)')
+
+args = None
+
+indentStack = [1]
+
+all_names = '''
+program
+typeDef
+ruleDef
+assumeStmt
+letStmt
+solveStmt
+'''.split()
+
+COLON = Literal(':').suppress()
+COMMA = Literal(',')
+LPA = Literal('(')
+RPA = Literal(')')
+BS = Literal('/')
+ENDL = LineEnd().suppress()
+pyType = Literal('int') ^ Literal('float')
+equalityOp = Literal(Names.equality)
+inequalityOp = Literal('<') ^ Literal('>') ^ Literal('<=') ^ Literal('>=')
+operator = (Literal('+') ^ Literal('-') ^ Literal('*') ^
+        Literal('/') ^ Literal('**')).setName('op')
+typeExt = Literal('+') ^ Literal('-')
+term = Word(alphanums + '_' + '.' + '-')
+
+component = Forward().setName('component')
+expression = Forward().setName('expression')
+constraint = Forward().setName('constraint')
+arg = Forward().setName('arg')
+arglist = Forward().setName('arglist')
+
+func = (term + arglist).setName('func')
+varName = (term + Optional(Literal('[') + Literal(']'))).setName('varName')
+equation = Group(expression + equalityOp + (expression ^ func)).setName('equation')
+constraint << (expression + inequalityOp + expression +
+    Optional((Literal('&&') | Literal('||')) + constraint))
+typeName = Group(term + Optional(typeExt))
+
+typeDecl = (Literal('typedef') + typeName(Names.typeName) + COLON +
+        pyType(Names.pyTypeName) + term(Names.shortName) + ENDL)
+typeBody = indentedBlock(constraint(Names.constraint) ^ equation(Names.equation),
+        indentStack)(Names.typeBody)
+typeDef = Group(typeDecl + typeBody).setResultsName(Names.typeDef, True)
+
+varDecl = (varName(Names.varName) + COLON + typeName(Names.typeName) +
+        Optional(Literal('as') + term(Names.shortName)))
+
+ruleDecl = (Literal('define') + term(Names.ruleName) + COLON + ENDL)
+ruleBody = indentedBlock(varDecl ^ constraint(Names.constraint) ^ equation(Names.equation),
+        indentStack)(Names.ruleBody)
+ruleDef = Group(ruleDecl + ruleBody).setResultsName(Names.ruleDef, True)
+
+list_struct = (Optional(term) + Literal('[') + arg +
+        ZeroOrMore(COMMA + arg) + Literal(']') +
+        Optional(Literal('|') +
+            Group(equation ^ constraint).setResultsName(Names.listCond, True)))
+tuple_struct = (LPA + arg + ZeroOrMore(COMMA + arg) + RPA).setName('tuple')
+struct = (list_struct ^ tuple_struct).setName('struct')
+
+assumeStmt = Group(Literal('assume') +
+        OneOrMore(term +
+            Optional(COMMA))(Names.assumedRule)).setResultsName(Names.assume, True)
+letStmt = (Suppress('let') + equation +
+        Optional(ZeroOrMore(COMMA.suppress() + equation))).setResultsName(Names.let, True)
+solveStmt = Group(Literal('explore') +
+        OneOrMore(term +
+            Optional(COMMA.suppress()))(Names.target)).setResultsName(Names.solve, True)
+analysisStmt = (assumeStmt | letStmt | solveStmt) + ENDL
+blankStmt = Suppress((LineStart() + LineEnd()) ^ White()).setName('blankStmt')
+commentStmt = (Literal('#') + restOfLine + ENDL).setName('comment')
+stmt = typeDef | ruleDef | analysisStmt | blankStmt | commentStmt
+program = OneOrMore(stmt)
+component << (term ^ varName ^ func ^ struct)
+arg << (component ^ expression ^ equation ^ struct)
+arglist << (LPA + Optional(arg + ZeroOrMore(COMMA + arg)) + RPA)
+expression << ((Optional(BS) + component +
+    ZeroOrMore(operator + expression)) ^ (LPA + expression + RPA))
+
+program.ignore(commentStmt)
+
+ast_nodes = []
+
+def do_solveStmt(s, l, t):
+    ast_nodes.append(SolveNode(t.asDict()))
+    return
+
+def do_letStmt(s, l, t):
+    ast_nodes.append(LetNode(t.asDict()))
+    return
+
+def do_assumeStmt(s, l, t):
+    ast_nodes.append(AssumeNode(t.asDict()))
+    return
+
+def do_typeDef(s, l, t):
+    ast_nodes.append(TypeNode(t.asDict()))
+    return
+
+def do_ruleDef(s, l, t):
+    ast_nodes.append(RuleNode(t.asDict()))
+    return
+
+def do_program(s, l, t):
+    program = Program(ast_nodes)
+    interp = Interpreter(program, args.z3core, args.draw, args.mcsamples)
+    #interp.test_gc_overhead()
+    interp.run()
+    return
+
+for name in all_names:
+    ex = vars()[name]
+    action = vars()['do_' + name]
+    ex.setName(name)
+    ex.setParseAction(action)
+
+def main():
+    global args
+    parser = get_parser()
+    addCommonOptions(parser)
+    addCompilerOptions(parser)
+    args = parse_args(parser)
+
+    if args.verbose:
+        typeDef.setDebug()
+        ruleDef.setDebug()
+        blankStmt.setDebug()
+        assumeStmt.setDebug()
+        letStmt.setDebug()
+        solveStmt.setDebug()
+        func.setDebug()
+        equation.setDebug()
+        constraint.setDebug()
+        arglist.setDebug()
+        arg.setDebug()
+        expression.setDebug()
+        component.setDebug()
+        inequalityOp.setDebug()
+        struct.setDebug()
+
+    with open(args.source, 'r') as src_file:
+        src = src_file.read()
+        src_file.close()
+    # Convert '=' to Eq in piecewise function, otherwise sympy cannot parse correctly.
+    src = COND_EQ.sub(lambda p: ', Eq(' + p.group(1) + ', ' + p.group(2) + '))', src)
+    # Convert '.' expresion to custom name clone extension
+    # because sympy cannot accept '.' in variable names.
+    src = NAME_EXT.sub(lambda p: p.group(1) + Names.clone_ext + \
+            Names.clone_ext.join(p.group(2).split('.')), src)
+    parsed_toks = program.parseString(src)
+
+if __name__ == '__main__':
+    main()
---
+++ interpreter/z3core.py
@@ -0,0 +1,150 @@
+### Assumptions ###
+# True: incoming edge
+# False: outgoing edge
+
+### Function Specification ###
+# SAT problem solved with the use of Z3
+# args = inputs: dictionary, equations: {int(eq_id), list of eq vars}
+# the function returns a dict {eq, outgoing edges}
+
+### Notes ###
+# Assertions can be used to verify that all inputs have been provided by the user
+
+import z3
+from timeit import default_timer as timer
+
+def graph_transform_z3(inputs, equations):
+
+	### Variables and SAT Equations Declaration ###
+	for inp in inputs:
+        	exec("%s = z3.Bool('%s')" % (inp, inp))
+		exec("%s_eq = z3.Bool('%s_eq')" % (inp, inp))
+
+	for eq, eq_vars in equations.iteritems():
+		exec("e%d = z3.Bool('e%d')" % (eq, eq))
+		for var in eq_vars:
+			exec("e%d%s = z3.Bool('e%d%s')" % (eq, var, eq, var))
+          
+
+	### Inputs ###
+	string_inputs = ""
+	for inp, value in inputs.iteritems():
+        	string_inputs += "%s == %s, " % (inp, str(value))
+	exec("problem = [%s]" % (string_inputs[:-2]))
+
+
+	### Equations ###
+	list_of_sat_eqs = []
+	string_eqs = ""
+
+	#*** SAT equations for system variables ***#
+	dict_var_edges = {} # Dictionary of edges per variable
+	for eq, eq_vars in equations.iteritems():
+		for var in eq_vars:
+			if (var) not in dict_var_edges:
+                                dict_var_edges[var] = []
+                                dict_var_edges[var].append(var)
+                        dict_var_edges[var].append("e%d%s" % (eq, var))
+	
+
+	# Variables can have 0 or 1 incoming edges
+	for var, list_of_var_edges in dict_var_edges.iteritems():
+        	pntr = 0
+        	string_var_edges = ""
+        	for i in range(len(list_of_var_edges)):
+                	string_var_edges += "z3.And("
+                	for pos, edge in enumerate(list_of_var_edges):
+                        	if pos == pntr:
+                                	string_var_edges += "%s, " % (edge) 
+                        	else:
+                                	string_var_edges += "z3.Not(%s), " % (edge)
+                	string_var_edges = "%s), " % (string_var_edges[:-2]) 
+                	pntr += 1
+		list_of_sat_eqs.append("%s_eq" % (var))
+        	string_eqs += "%s_eq == z3.Or(%s), " % (var, string_var_edges[:-2]) 
+
+
+	#*** System equations in SAT format ***#
+
+	# Equations can have 0 or 1 outgoing edges
+	for eq, eq_vars in equations.iteritems():
+		pntr = 0
+		string_eq_edges = ""
+        	for i in range(len(eq_vars)+1):
+                	string_eq_edges += "z3.And("
+                	for pos, var in enumerate(eq_vars):
+                        	if pos == pntr:
+                                	string_eq_edges += "e%d%s, " % (eq, var)
+                        	else:
+                                	string_eq_edges += "z3.Not(e%d%s), " % (eq, var)
+                	string_eq_edges = "%s), " % (string_eq_edges[:-2])
+                	pntr += 1
+
+		list_of_sat_eqs.append("e%d" % (eq))
+		string_eqs += "e%d == z3.Or(%s), " % (eq, string_eq_edges[:-2])
+	
+ 
+	#*** Final SAT condition ***#
+	string_all_sat_eqs = ""
+	for sat_eq in list_of_sat_eqs:
+        	string_all_sat_eqs += "%s, " % (sat_eq) 
+	string_eqs += "z3.And(%s) == True" % (string_all_sat_eqs[:-2])
+
+	exec("equations_SAT = [%s]" % (string_eqs))        
+
+
+	#*** Return solution as a dict ***#
+	solution_z3object, time = mod_solve(equations_SAT + problem)
+	if solution_z3object:
+		solution_list = str(solution_z3object)[1:-1].replace(' ','').split(',\n')
+		solution_CHARM = {}
+		for eq, eq_vars in equations.iteritems():
+			for var in eq_vars:
+				if "e%d%s=True" % (eq, var) in solution_list:
+					solution_CHARM[eq] = var
+			if (eq) not in solution_CHARM:
+				solution_CHARM[eq] = None
+		return solution_CHARM, time
+	else:
+		return None, time
+
+
+# Slightly modified version of the solve function found in z3.py
+def mod_solve(*args, **keywords):
+	s = z3.Solver()
+    	s.set(**keywords)
+    	s.add(*args)
+    	if keywords.get('show', False):
+        	print(s)
+        start = timer()
+    	r = s.check()
+        end = timer()
+        time = end - start
+    	if r == z3.unsat:
+        	print("Cannot convert to functional graph.")
+		return None, time
+    	elif r == z3.unknown:
+        	print("Failed to convert to functional graph.")
+        	try:
+            		print(s.model())
+			return None, time
+        	except z3.Z3Exception:
+            		return None, time
+    	else:
+		#print(s.model())
+        	return s.model(), time
+
+def test():
+    ### Example 1 - unsat ### 
+    inputs = {'a': True, 'b': False, 'x': False, 'y': False}
+    equations = {1: ['x', 'a', 'b'], 2: ['y', 'x', 'a']}
+    #check_config(inputs, equations)
+    print check_config(inputs, equations)
+
+    ### Example 2 - sat ###
+    inputs = {'a': True, 'b': False, 'x': True, 'y': False}
+    equations = {1: ['x', 'a', 'b'], 2: ['y', 'x', 'a']}
+    #check_config(inputs, equations)
+    print check_config(inputs, equations)
+
+#test()
